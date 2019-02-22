af0c16f33d545453b52779b9043c8cbb56a6831e
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-22 11:22:58 -0700

    Added post endpoint to actions router

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -24,4 +24,18 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: 'Could not retrieve the action with the specified ID at this time', error: err}))
 })
 
+router.post('/', (req, res) => {
+  if(req.body.description !== undefined && req.body.completed !== undefined && req.body.project_id !== undefined){
+    db.post(table, req.body)
+      .then(id => {
+        db.getById(table, id[0])
+          .then(newAction => res.status(201).json(newAction))
+          .catch(err => res.status(500).json({errorMessage: 'Could not retrieve the new action at this time', error: err}))
+      })
+      .catch(err => res.status(500).json({errorMessage: 'Could not create an action at this time', error: err}))
+  } else {
+    res.status(400).json({errorMessage: `Please provide${!req.body.description ? ' a description' : ''}${!req.body.description && req.body.completed === undefined || !req.body.description && req.body.project_id === undefined || req.body.project_id === undefined && req.body.completed === undefined  ?' and ' : ''}${req.body.completed === undefined ? ' a completed flag ' : ''}${req.body.completed === undefined && req.body.description === undefined && req.body.project_id === undefined ? 'and ' : ''}${req.body.project_id === undefined ? 'a valid project ID' : ''}.`});
+  }
+})
+
 module.exports = router;
